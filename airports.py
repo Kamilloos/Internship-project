4383765d2181bd53c8345e2e02a99968e0d9b091
Kamilloos <45430631+Kamilloos@users.noreply.github.com>
2020-06-10 15:21:03 +0200

    Update airports.py

--- airports.py
+++ airports.py
@@ -5,9 +5,9 @@ from gremlin_python.driver.driver_remote_connection import DriverRemoteConnectio
 from itertools import islice
 
 #starting connection
-graph = Graph()
-connection = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
-g = graph.traversal().withRemote(connection)
+graph=Graph()
+connection=DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
+g=graph.traversal().withRemote(connection)
 
 print "Start: Drop all vertices"
 g.V().drop().iterate()
@@ -15,16 +15,16 @@ print "Finish: Drop all vertices"
 
 filename="airports.dat"
 
-number_of_lines = 7698#define the number of lines to read (MAX: 7698)
+number_of_lines=7698#define the number of lines to read (MAX: 7698)
 
-vertices = {}
+vertices={}
 
 #row[0] - ID //// row[1] - name //// row[3] - country
-with open(filename, 'r') as input_file:
-    lines_cache = islice(input_file, number_of_lines)
-    print "Next ", number_of_lines, " batch"
+with open(filename,'r') as input_file:
+    lines_cache=islice(input_file, number_of_lines)
+    print "Next",number_of_lines,"batch"
     for current_line in lines_cache:
-        row = current_line.split(',')
+        row=current_line.split(',')
 
         name=row[1]
         name=name[slice(1,len(name)-1)]#removing quation marks
@@ -39,20 +39,20 @@ with open(filename, 'r') as input_file:
 
 filename="routes.dat"
 
-number_of_lines = 67658#define the number of lines to read (MAX: 67658)
+number_of_lines=67658#define the number of lines to read (MAX: 67658)
 
-with open(filename, 'r') as input_file:
-    lines_cache = islice(input_file, number_of_lines)
-    print "Next ", number_of_lines, " batch"
+with open(filename,'r') as input_file:
+    lines_cache=islice(input_file, number_of_lines)
+    print "Next",number_of_lines,"batch"
     for current_line in lines_cache:
-        row = current_line.split(',')
+        row=current_line.split(',')
 
         print "Add route from",row[2],'to',row[4]#row[2] - source //// row[4] - destination
 
         try:
-        g.V(vertices[row[2]]).addE('route').to(vertices[row[4]]).iterate()
+            g.V(vertices[row[2]]).addE('route').to(vertices[row[4]]).iterate()
         except KeyError:
-        print 'Warrning! KeyError:',row[2],'or KeyError:',row[4]
+            print 'Warrning! KeyError:',row[2],'or KeyError:',row[4]
 
 #closing connection
 connection.close()
